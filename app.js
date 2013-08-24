63f8d36444fefdf59eb474cad36d2f6ca0c254f0
saulo da silva santiago <saulodasilvasantiago@gmail.com>
2013-08-24 16:26:54 -0300

    incluindo express load e fazendo a view de login

--- app.js
+++ app.js
@@ -1,35 +1,16 @@
+var express = require('express')
+  , load = require('express-load')
+  , app = express();
 
-/**
- * Module dependencies.
- */
-
-var express = require('express');
-var routes = require('./routes');
-var user = require('./routes/user');
-var http = require('http');
-var path = require('path');
-
-var app = express();
-
-// all environments
-app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.bodyParser());
-app.use(express.methodOverride());
-app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname + '/public'));
 
-// development only
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
-}
+load('models')
+   .then('controllers')
+   .then('routes')
+   .into(app);
 
-app.get('/', routes.index);
-app.get('/users', user.list);
-
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+app.listen(3000, function(){
+  console.log("Ntalk no ar.");
 });
