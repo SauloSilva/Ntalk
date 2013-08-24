eedf29b14511048f91841739cf122229497bce4e
saulo da silva santiago <saulodasilvasantiago@gmail.com>
2013-08-24 15:52:38 -0300

    removendo tudo

--- rails/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-ProjectName::Application.config.secret_token = '20d29e5681218d0813a61f460fecfebf9ba69b1264a5ea2dcf86f6ab954c31bf18ed2a1a97ce83ea17ed219b14c088d66826d290ab9fc6eac407870cd18a8784'
